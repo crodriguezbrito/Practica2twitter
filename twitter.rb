16d2cbdc88fd954cb0828f86f08d375ee437f80d
Sonia <carlos@ubuntu.(none)>
2013-10-01 19:13:45 +0100

    probandoconejemplo

---
+++ twitter.rb
@@ -0,0 +1,35 @@
+require 'rack'
+require 'pry-debugger'
+
+class HelloWorld
+
+  def call env
+    req = Rack::Request.new(env)
+    res = Rack::Response.new 
+    binding.pry if ARGV[0]
+    res['Content-Type'] = 'text/html'
+    name = (req["firstname"] && req["firstname"] != '') ? req["firstname"] :'World'
+    res.write <<-"EOS"
+      <!DOCTYPE HTML>
+      <html>
+        <title>Rack::Response</title>
+        <body>
+          <h1>
+             Hello #{name}!
+             <form action="/" method="post">
+               Your name: <input type="text" name="firstname" autofocus><br>
+               <input type="submit" value="Submit">
+             </form>
+          </h1>
+        </body>
+      </html>
+    EOS
+    res.finish
+  end
+end
+
+Rack::Server.start(
+  :app => HelloWorld.new,
+  :Port => 9292,
+  :server => 'thin'
+)
